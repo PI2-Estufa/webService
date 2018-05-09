1b2221b6537c8e394a28655fd2c040158cd4972e
adailson2 <adailson2@gmail.com>
2018-05-09 19:58:00 -0300

    Stablish connection to rabbit in flask

--- server.py
+++ server.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from kombu import Connection, Queue, Exchange, Consumer
 
 app = Flask(__name__)
 
+temperature = 0
+
+def callback(body, msg):
+    global temperature
+    temperature = body
+    msg.ack()
+
+
+conn = Connection("amqp://rabbit")
+exchange = Exchange("main", "direct", durable=True)
+queue = Queue("temperature_queue", exchange=exchange)
+consumer = Consumer(channel=conn, queues=[queue], callbacks=[callback])
+
+
 @app.route("/")
 def index():
-    return "Hello World"
+    conn.connect()
+    consumer.consume()
+    conn.drain_events()
+    return str(temperature)
