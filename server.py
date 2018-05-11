8fd73dae0dc59e22ae32a37f66fbbf025bfd5ca6
Adailson Santos <adailson2@gmail.com>
2018-05-11 17:37:05 -0300

    Resolving stop queue

--- server.py
+++ server.py
@@ -9,28 +9,29 @@ temperatures = []
 def callback(body, msg):
     global temperatures
     temperatures.append(body)
-    msg.ack()
-
-
-conn = Connection("amqp://rabbit")
-exchange = Exchange("main", "direct", durable=True)
-queue = Queue("temperature_queue", exchange=exchange)
-consumer = Consumer(channel=conn, queues=[queue], callbacks=[callback])
+#    msg.ack()
 
 
 @app.route("/")
 def index():
     global temperatures
     temperatures = []
-    conn.connect()
+
+    conn = Connection("amqp://rabbit")
+    exchange = Exchange("main", "direct", durable=True)
+    queue = Queue("temperature_queue", exchange=exchange)
+    consumer = Consumer(channel=conn, queues=[queue], callbacks=[callback])
+
+    if not conn.connected:
+        conn.connect()
+    
     consumer.consume()
+    
     for i in list(range(5)):
         try:
             conn.drain_events(timeout=1)
         except:
-            response = { "temperatures" : temperatures }
-            return jsonify(response)
-    
+            break
     response = { "temperatures" : temperatures }
-    
+    conn.release()
     return jsonify(response)
