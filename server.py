e99df9ed1812fcc4bd7cfe3ddaadcf74b91931ca
Lucas Amoêdo <lucas.advc@gmail.com>
2018-06-22 13:21:26 -0300

    so many things

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from flask import Flask, jsonify, request
 from flask_jwt import JWT, jwt_required
 from flask_marshmallow import Marshmallow
 from werkzeug.utils import secure_filename
+from kombu import Connection, Exchange, Queue
+from kombu.pools import producers
 from datetime import timedelta
 from datetime import date
 import datetime
@@ -14,8 +16,8 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-UPLOAD_FOLDER = "./uploads"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'ameixa'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=36000)
 ma = Marshmallow(app)
@@ -37,16 +39,12 @@ def authenticate(username, password):
         return None
 
 def identity(payload):
-    user = db.session.query(db.User).filter_by(id=payload["identity"]).first()
+    user = db.session.query(db.User).filter_by(id=payload['identity']).first()
     return user
 
 jwt = JWT(app, authenticate, identity)
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
-
-@app.route("/")
+@app.route('/')
 @jwt_required()
 def index():
     temperature_query = db.session.query(db.Temperature).order_by(db.Temperature.id.desc()).limit(5)
@@ -71,46 +69,46 @@ def index():
     drawer_statuses = [d.value for d in drawer_status_query]
 
     images = os.listdir('./uploads')
-    images.remove(".keep")
+    images.remove('.keep')
 
     response = {
-        "temperatures": temperatures,
-        "humidities": humidities,
-        "pHs": pHs,
-        "iluminations": iluminations,
-        "water_temperatures": water_temperatures,
-        "water_levels": water_levels,
-        "drawer_statuses": drawer_statuses,
-        "images": images,
-        "warnings": [
+        'temperatures': temperatures,
+        'humidities': humidities,
+        'pHs': pHs,
+        'iluminations': iluminations,
+        'water_temperatures': water_temperatures,
+        'water_levels': water_levels,
+        'drawer_statuses': drawer_statuses,
+        'images': images,
+        'warnings': [
             {
-                "id": 2,
-                "message": "PH elevada!",
-                "level": "error"
+                'id': 2,
+                'message': 'PH elevada!',
+                'level': 'error'
             }
         ]
     }
     return jsonify(response)
 
 
-@app.route("/picture", methods=["POST"])
+@app.route('/picture', methods=['POST'])
 def pictures():
-    if "picture" not in request.files:
-        return "No file in request"
+    if 'picture' not in request.files:
+        return 'No file in request'
 
-    file = request.files["picture"]
+    file = request.files['picture']
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return filename
 
 @app.route('/report/<report>')
 @jwt_required()
 def temperatures(report):
     entities = {
-        "temperatures": db.Temperature,
-        "humidities": db.Humidity,
-        "phs": db.Ph,
-        "water_temperatures": db.WaterTemperature
+        'temperatures': db.Temperature,
+        'humidities': db.Humidity,
+        'phs': db.Ph,
+        'water_temperatures': db.WaterTemperature
     }
 
     # path = 
@@ -118,21 +116,21 @@ def temperatures(report):
     try:
         Entity = entities[report]
     except KeyError:
-        return jsonify({"error": True, "message": "invalid route"})
+        return jsonify({'error': True, 'message': 'invalid route'})
 
-    allowed_periods = ["day", "week", "month", "year", "lifespan"]
+    allowed_periods = ['day', 'week', 'month', 'year', 'lifespan']
 
-    period = request.args.get("period", "week")
+    period = request.args.get('period', 'week')
 
     if period not in allowed_periods:
-        return jsonify({"error": True, "message": "invalid period"})
+        return jsonify({'error': True, 'message': 'invalid period'})
 
     time_patrol = {
-        "day": 0,
-        "week": 7,
-        "month": 30,
-        "year": 365,
-        "lifespan": 365 * 79
+        'day': 0,
+        'week': 7,
+        'month': 30,
+        'year': 365,
+        'lifespan': 365 * 79
     }
     passdate = datetime.date.today() - timedelta(days=time_patrol[period])
     results = db.session.query(Entity.id, Entity.value, Entity.created_date).filter(
@@ -140,13 +138,13 @@ def temperatures(report):
     values = [r.value for r in results]
     average = round(sum(values)/results.count(), 2)
     
-    response = [{"id":t.id, "value":t.value, "date": t.created_date} for t in results]
+    response = [{'id':t.id, 'value':t.value, 'date': t.created_date} for t in results]
     return jsonify({
-        "average": average,
-        "results": response
+        'average': average,
+        'results': response
     })
 
-@app.route("/plant", methods=["POST"])
+@app.route('/plant', methods=['POST'])
 def add_plant():
      specie = request.json['specie']
      created_at = request.json['created_at']
@@ -159,14 +157,14 @@ def add_plant():
                     created_at= new_plant.created_at,
                     observations= new_plant.observations)
 
-@app.route("/plant/<id>", methods=["GET"])
+@app.route('/plant/<id>', methods=['GET'])
 def plant_detail(id):
     plant = db.session.query(db.Plant).get(id)
     return jsonify(specie= plant.specie,
                    created_at= plant.created_at,
                    observations= plant.observations)
 
-@app.route("/plant/<id>", methods=["PUT"])
+@app.route('/plant/<id>', methods=['PUT'])
 def plant_update(id):
     plant = db.session.query(db.Plant).get(id)
     specie = request.json['specie']
@@ -180,22 +178,37 @@ def plant_update(id):
     db.session.commit()
     return plant_schema.jsonify(plant)
 
-@app.route("/plant/<id>", methods=["DELETE"])
+@app.route('/plant/<id>', methods=['DELETE'])
 def plant_delete(id):
     plant = db.session.query(db.Plant).get(id)
     db.session.delete(plant)
     db.session.commit()
     return plant_schema.jsonify(plant)
 
-@app.route("/plant", methods=["GET"])
+@app.route('/plant', methods=['GET'])
 def plant_all():
     plant_query = db.session.query(db.Plant).order_by(db.Plant.id.desc()).limit(30)
-    response = [{"id":p.id, "specie":p.specie, "observations": p.observations} for p in plant_query]
+    response = [{'id':p.id, 'specie':p.specie, 'observations': p.observations} for p in plant_query]
     return jsonify(response)
 
 app.config.update(dict(
-    SECRET_KEY="powerful secretkey",
-    WTF_CSRF_SECRET_KEY="a csrf secret key"
+    SECRET_KEY='powerful secretkey',
+    WTF_CSRF_SECRET_KEY='a csrf secret key'
 ))
 
+@app.route('/drawer', methods=['GET'])
+def drawer_control():
+    drawer_exchange = Exchange('rdrawer', type='direct')
+    # task_queues = [Queue('rdrawer', drawer_exchange, routing_key='rdrawer')]
+    connection = Connection(os.environ.get('RABBIT_URL'))
+    command = '0'
+    payload = {'command': command}
+    routing_key = 'rdrawer'
+    with producers[connection].acquire(block=True) as producer:
+        producer.publish(payload,
+                        exchange=drawer_exchange,
+                        declare=[drawer_exchange],
+                        routing_key=routing_key)
+
+    return jsonify({"status": "ok"})
 
